bf6640e59c570a9c151309ade356a824e7cead9c
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-01 11:50:34 +0300

    writes html for header

--- src/components/front/header/Header.js
+++ src/components/front/header/Header.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Logo from '../../../images/logo.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className='header'>
+        <img src={Logo} alt="logo" className='logo'/>
+        <nav>
+        <ul>
+            <li>Product</li>
+            <li>Company</li>
+            <li>Connect</li>
+        </ul>
+        </nav>
+
+        <button className='login'>Login</button>
+        <button className='signup'>Sign Up</button>
+        <h1>A modern publishing platform</h1>
+        <p className='online-brand'>Grow you audience and build your online brand</p>
+
+    </div>
+    </header>
+    
   )
 }
 

11c382345a720f1610da0e58f2a69b2590e8956a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-01 13:19:25 +0300

    styles header component

--- src/components/front/header/Header.js
+++ src/components/front/header/Header.js
@@ -6,22 +6,28 @@ const Header = () => {
     return (
         <header >
             <div className="header">
-            <div className='upper-header-section'>
-                <img src={Logo} alt="logo" className='logo' />
-                <nav>
-                    <ul>
-                        <li>Product</li>
-                        <li>Company</li>
-                        <li>Connect</li>
-                    </ul>
-                </nav>
-                <button className='login'>Login</button>
-                <button className='signup'>Sign Up</button>
-            </div>
-            <h1>A modern publishing platform</h1>
-            <p className='online-brand'>Grow you audience and build your online brand</p>
-            <button>Start For Free</button>
-            <button>Learn More</button>
+                <div className='header-upper-section'>
+                    <img src={Logo} alt="logo" className='logo' />
+                    <nav>
+                        <ul>
+                            <li>Product</li>
+                            <li>Company</li>
+                            <li>Connect</li>
+                        </ul>
+                    </nav>
+                    <button className='login'>Login</button>
+                    <button className='signup'>Sign Up</button>
+                </div>
+                <div className="header-text">
+                    <h1>A modern publishing platform</h1>
+                    <p className='online-brand'>Grow you audience and build your online brand</p>
+                </div>
+
+                <div className="header-button-container">
+                    <button className='start-for-free'>Start For Free</button>
+                    <button className='learn-more'>Learn More</button>
+                </div>
+
             </div>
         </header>
 

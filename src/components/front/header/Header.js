e08120d775fd9b1e1cd27ed081dc640345732efe
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-01 11:55:16 +0300

    finishes up on html for header

--- src/components/front/header/Header.js
+++ src/components/front/header/Header.js
@@ -2,27 +2,29 @@ import React from 'react'
 import Logo from '../../../images/logo.svg'
 
 const Header = () => {
-  return (
-    <header>
-        <div className='header'>
-        <img src={Logo} alt="logo" className='logo'/>
-        <nav>
-        <ul>
-            <li>Product</li>
-            <li>Company</li>
-            <li>Connect</li>
-        </ul>
-        </nav>
+    return (
+        <header>
+            <div className='header'>
+                <img src={Logo} alt="logo" className='logo' />
+                <nav>
+                    <ul>
+                        <li>Product</li>
+                        <li>Company</li>
+                        <li>Connect</li>
+                    </ul>
+                </nav>
+                <button className='login'>Login</button>
+                <button className='signup'>Sign Up</button>
+            </div>
+            <h1>A modern publishing platform</h1>
+            <p className='online-brand'>Grow you audience and build your online brand</p>
+            <button>Start For Free</button>
+            <button>Learn More</button>
 
-        <button className='login'>Login</button>
-        <button className='signup'>Sign Up</button>
-        <h1>A modern publishing platform</h1>
-        <p className='online-brand'>Grow you audience and build your online brand</p>
 
-    </div>
-    </header>
-    
-  )
+        </header>
+
+    )
 }
 
 export default Header

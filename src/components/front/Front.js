ff37672ee2380cddf787378fc73ade41a6570b20
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-02 11:06:44 +0300

    works on making the components responsive using percentages

--- src/components/front/Front.js
+++ src/components/front/Front.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from './header/Header'
 import Main from './main/Main'
 import Footer from './footer/Footer'
+import './front.css'
 
 const Front = () => {
   return (
-    <div>
+    <div className='entire-page-container'>
     <Header/>
     <Main/>
     <Footer/>
--- src/components/front/footer/Footer.js
+++ src/components/front/footer/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import FooterImage from '../../../images/illustration-phones.svg'
+import './footer.css'
 
 const Footer = () => {
   return (
     <div className="footer">
-        <img src={FooterImage} alt="illustration phones" />
+        <img src={FooterImage} alt="illustration phones"  className='footer-image'/>
+    
         <div className="footer-inner-container">
-            <p>State of the Art Infrastructure</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis est id eum exercitationem facilis quasi nam sint odit, reprehenderit cupiditate veniam et quas excepturi maiores recusandae. Cumque dolorem dolor sint.</p>
+            <h3 className='footer-heading'>State of the Art Infrastructure</h3>
+            <p className='footer-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis est id eum exercitationem facilis quasi nam sint odit, reprehenderit cupiditate veniam et quas excepturi maiores recusandae. Cumque dolorem dolor sint.</p>
         </div>
 
     </div>
